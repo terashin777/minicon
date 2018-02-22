8884d035f99416926f2925c09a1333887bd57340
terashin777 <20200386+terashin777@users.noreply.github.com>
2018-02-22 08:41:48 +0000

    Create actions create, destroy and update and create views edit, new and show

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,13 +1,42 @@
 class Admin::EventsController < ApplicationController
+  layout 'admin'
+  
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+    @event = Event.new
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
+  
+  def create
+    event = Event.new(event_params)
+    event.save
+    redirect_to admin_event_path(event), notice: "Event was successfully created!"
+  end
+  
+  def destroy
+    Event.destroy(params[:id])
+    redirect_to admin_events_path, notice: "Event was successfully destroyed!"
+  end
+  
+  def update
+    Event.update(event_params)
+    redirect_to admin_events_path, notice: "Event was successfully updated!"
+  end
+  
+  private
+  
+  def event_params
+    params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+  end
+  
 end
